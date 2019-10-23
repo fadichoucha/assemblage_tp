8d4c03e03700ddd61897065a5f65b42f0db38c20
fadichoucha <fchoucha@hotmail.com>
2019-10-23 03:21:35 +0200

    Next step

--- debruij.py
+++ debruij.py
@@ -9,20 +9,18 @@ def read_fastq(i):
     """ take a fasta file as input and return an iteratable object contains
     all sequences
     """
-    
     with open(i, mode='r') as file_in:
         for line in file_in:
             yield next(file_in)
             next (file_in)
             next (file_in)
 
-seq_iterate = read_fastq('data/eva71_hundred_reads.fq')
+#  seq_iterate = read_fastq('data/eva71_hundred_reads.fq')
 
 
     
 def cut_kmer(seq, k=21) : 
-    """ take a sequence (seq) geenrated from the the function read_fastq.
-        k is the wanted length of kmers
+    """ ;
     """
     l = len(seq)
 
@@ -36,28 +34,25 @@ def cut_kmer(seq, k=21) :
 
 
     
-def build_kmer_dict(fastq, kmer_len):
-    
+def build_kmer_dict(fastq, kmers_len):
     k_dict ={}
-    for one_seq in fastq:
-        for one_kmer in cut_kmer(one_seq, kmer_len):
+    seq_list = [seq[:-1] for seq in read_fastq(fastq)]
+    for seq in seq_list:
+        for one_kmer in cut_kmer(seq, kmers_len):
             if one_kmer not in k_dict.keys():
                 k_dict[one_kmer] = 1
-            elif one_kmer in k_dict.keys():
-                k_dict[one_kmer] = k_dict[one_kmer] +1
             else:
-                print('Error')
-
+                k_dict[one_kmer] += 1
     return k_dict
 
-kmers_dict = build_kmer_dict(fastq=seq_iterate, kmer_len=3)
+kmers_dict = build_kmer_dict(fastq='/home/fadi/git/assemblage_tp/data/eva71_hundred_reads.fq', kmers_len=21)
 print(len(kmers_dict))
 
 
 def build_graph(kmer_dict):
     graphe = networkx.DiGraph()
-    for j in kmer_dict:
-        graphe.add_edge(j[:-1], j[1:], weight=kmer_dict[j])
+    for key, value in kmer_dict.items():
+        graphe.add_edge(key[:-1], key[1:], weight=value)
     return graphe
 
 graphe_result =build_graph(kmers_dict)    
@@ -68,12 +63,13 @@ print(len(graphe_result.nodes()))
 def get_starting_nodes(graphe):
     starting_nodes_list = []
     for start_node in graphe.nodes:
+        #print(len(list(graphe.predecessors(start_node))))
         if len(list(graphe.predecessors(start_node))) == 0:
             starting_nodes_list.append(start_node)
     return starting_nodes_list        
     
 x = get_starting_nodes(graphe_result)
-print(x)
+print(len(x))
 
 def get_sink_nodes(graphe):
     sink_nodes_list = []
@@ -83,7 +79,7 @@ def get_sink_nodes(graphe):
     return sink_nodes_list
 
 y = get_sink_nodes(graphe_result)
-print(y)
+print(len(y))
  
 def get_contigs(graphe, inputs, outputs):
     pass
